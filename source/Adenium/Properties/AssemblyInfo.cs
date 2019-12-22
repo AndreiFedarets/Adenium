f26e8dbb1436035257a572f1250dea7d9f4c135e
Andrei Fedarets <andrei.fedarets@gmail.com>
2019-12-23 01:15:22 +0300

    Nuget preparing

--- source/Adenium/Properties/AssemblyInfo.cs
+++ source/Adenium/Properties/AssemblyInfo.cs
@@ -10,13 +10,9 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andrei Fedarets")]
 [assembly: AssemblyProduct("Adenium")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 Andrei Fedarets")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 //In order to begin building localizable applications, set 
@@ -38,16 +34,5 @@ using System.Windows;
                                       // app, or any theme specific resource dictionaries)
 )]
 
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
